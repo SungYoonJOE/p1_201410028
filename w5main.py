3c59a912b008760a21ef8498bf1b04057e43c6d9
조성윤 p1_201410028 <hahajsu@naver.com>
2016-04-04 13:51:13 +0900

    Added files via upload

---
+++ w5main.py
@@ -0,0 +1,24 @@
+﻿def computeBMI(height,weight):
+        height=1.8
+        weight=75
+        bmi=weight/(height*height)
+        print bmi
+        bmi=24.99
+        if bmi>=18.5 and bmi<24.9:
+           print "normal weight"
+        elif bmi>=25.0 and bmi<=29.9:
+            print "overweight"
+        else:
+            print "error"
+
+
+def lab5():
+     height=1.8
+     weight=75
+     computeBMI(height,weight)
+
+def main():
+        lab5()
+
+if__name__=="__main__"
+        "main"
