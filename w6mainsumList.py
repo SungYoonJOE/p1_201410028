416c81f54e23069ae2e8d245d528b3e60d99974d
조성윤 p1_201410028 <hahajsu@naver.com>
2016-04-11 13:54:40 +0900

    w6mainsumlist.py

---
+++ w6mainsumList.py
@@ -0,0 +1,34 @@
+﻿x3=list()
+for i in range(0,1001):
+    if i%4==0 and i%5!=0:
+        x3.append(i)
+print x3
+
+sum=0
+for i in range(0,len(x3)):
+    sum+=x3[i]
+print sum
+
+
+
+"""
+JSY 201410028
+20160411
+"""
+
+def sumList(list):
+    sum=0
+    for i in range(0,len(x3)):
+        sum+=x3[i]
+    return sum
+    
+sumList(x3)
+
+
+def lab6():
+    x=[1,2,3]
+    mysum=sumList(x3)
+    print mysum
+
+def main():
+    lab6()
